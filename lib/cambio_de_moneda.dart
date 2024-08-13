d6bb92d378ed298a824078d94211d025979d36c1
Roberto Moran <byrobertomoran@gmail.com>
2024-08-12 21:19:57 -0600

    avance

--- lib/cambio_de_moneda.dart
+++ lib/cambio_de_moneda.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
 class CambioDeMoneda extends StatefulWidget {
+
   const CambioDeMoneda({super.key});
 
   @override
@@ -10,6 +11,9 @@ class CambioDeMoneda extends StatefulWidget {
 }
 
 class _CambioDeMonedaState extends State<CambioDeMoneda> {
+    TextEditingController controller = TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,23 +32,48 @@ class _CambioDeMonedaState extends State<CambioDeMoneda> {
                   SizedBox(width: 30.0),
                   ElevatedButton(onPressed: () {}, child: Text('Euro')),
                 ],
-                
               ),
               Padding(padding: EdgeInsets.all(20.0)),
               Row(
-                
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  
-
                   Text('Compra'),
                   SizedBox(width: 30.0),
                   Text('|'),
-                   SizedBox(width: 30.0),
+                  SizedBox(width: 30.0),
                   Text('Venta'),
                 ],
               ),
+              Padding(padding: EdgeInsets.all(10.0)),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('24.6834'),
+                      SizedBox(width: 50.0),
+                      Text('24.8068'),
+                    ],
+                  ),
+                ],
+              ),
+              Padding(padding: EdgeInsets.all(10.0)),
+              SizedBox(
+                height: 50.0,
+                width: 100.0,
+                child: TextField(
+                  controller: TextEditingController(),
+                  readOnly: true,
+                  decoration: InputDecoration(labelText: 'USD'),
+                  onChanged: (value) {
+                    setState(() {
+                      
+                    });
+                  },
+                ),
+              )
             ],
+            
           )),
     );
   }
